5a70d177100df2f463f10ac1a2fb164cfa95850c
AdamR1234 <adam-@msn.com>
2019-12-23 13:26:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ leftElement.innerHTML = "</br><span style='color:#757575;'>&lt;</span>\
 <span style='color:#757575;'>&lt;</span>\
 <span style='color:#4a8acc;'>p</span>\
 <span style='color:#89e1fd';> style</span>=\
-<span style='color:#d28a68'>'padding: 1px; max-width:80%;'</span>\
+<span style='color:#d28a68'>'padding: 0px; max-width:80%;'</span>\
 <span style='color:#757575;'>&gt;</span></br></br>\
 <textarea placeholder='Enter Content Here' rows='4' cols='50' id='content"+contentCount+"'></textarea>\
 </br></br><span style='color:#757575;'>&lt;/</span>\
@@ -78,14 +78,14 @@ content.style.fontSize = "13px";
 
 document.getElementById("title"+h3Count).addEventListener("keyup",function(){
 
-    rightElement.innerHTML = "<center><h3>" + title.value + "</h3>" + "<p style='padding: 1px; max-width:80%;font-size:13px;'>" + content.value.replace(/\n/g, "") + "</p>";
-    bottomElement.innerHTML = "&lt;h3 style='color: #000000; font-size: 18px; text-transform: none; font-weight: bold;'&gt;" + title.value + "&lt;/h3&gt;" + "&lt;p style='padding: 1px; max-width:80%;'&gt;" + content.value.replace(/\n/g, "") + "&lt;/p&gt;";
+    rightElement.innerHTML = "<center><h3>" + title.value + "</h3>" + "<p style='padding: 0px; max-width:80%;font-size:13px;'>" + content.value.replace(/\n/g, "") + "</p>";
+    bottomElement.innerHTML = "&lt;h3 style='color: #000000; font-size: 18px; text-transform: none; font-weight: bold;'&gt;" + title.value + "&lt;/h3&gt;" + "&lt;p style='padding: 0px; max-width:80%;'&gt;" + content.value.replace(/\n/g, "") + "&lt;/p&gt;";
 
 });
 document.getElementById("content"+contentCount).addEventListener("keyup",function(){
     bottom.style.fontSize = "1em";
-    rightElement.innerHTML = "<center><h3>" + title.value + "</h3>" + "<p style='padding: 1px; max-width:80%;font-size:13px;'>" + content.value.replace(/\n/g, "") + "</p>";
-    bottomElement.innerHTML = "&lt;h3 style='color: #000000; font-size: 18px; text-transform: none; font-weight: bold;'&gt;" + title.value + "&lt;/h3&gt;" + "&lt;p style='padding: 1px; max-width:80%;'&gt;" + content.value.replace(/\n/g, "") + "&lt;/p&gt;";
+    rightElement.innerHTML = "<center><h3>" + title.value + "</h3>" + "<p style='padding: 0px; max-width:80%;font-size:13px;'>" + content.value.replace(/\n/g, "") + "</p>";
+    bottomElement.innerHTML = "&lt;h3 style='color: #000000; font-size: 18px; text-transform: none; font-weight: bold;'&gt;" + title.value + "&lt;/h3&gt;" + "&lt;p style='padding: 0px; max-width:80%;'&gt;" + content.value.replace(/\n/g, "") + "&lt;/p&gt;";
 });
 
 }
